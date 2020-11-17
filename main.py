8a8b0049ef33f899bf4d688dd609f16b6da14f1f
Natalie Opritza <nopritza24@biomedscienceacademy.org>
2020-11-17 18:22:27 +0000

    I changed the grammer and punctuation since the spacing wasn't equal and the commas and periods were sometimes in the wrong spaces.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 """
 MadLibs
 Author: 
-Period/Core:
-
-
+Period/Core:5
 """
+print ("Let's play Silly Sentences!")
+boys_name = input("Enter boy's name: ")
+noun = input("Enter a noun: ")
+any_food = input("Enter any food: ")
+adjective = input("Enter an adjective: ")
+animal = input("Enter any animal: ")
+hobby = input("Enter any hobby: ")
+verb = input("Enter any verb: ")
+any_speed = input("Enter any speed: ")
+print ("There are many " + animal + " out here on the pasture. " + boys_name + " takes care of them every single day with his amazing " + any_food + " and " + adjective + " mind. When he isn't taking care of \"" + animal + "\", he likes to spend his time " + hobby +". " + boys_name + ", " + verb + " toward the back of his farm. He walked and walked until he spotted " + noun + ". " + noun + " looked very scary. " + boys_name + " " + verb + " toward it as " + any_speed + " as he could. As soon as he approached " + noun + ", it jumped right at him and threw his " + animal + " right in his face.")
